ff0e483b2f38049229806299cde72c1f9ac8f402
editorbank <>
2022-01-05 11:01:51 +0300

    add if to 2.sh

--- 2.sh
+++ 2.sh
@@ -3,10 +3,12 @@
 ssh -p 8822 git@localhost ls -lA
 ssh -p 8822 git@localhost git init --bare repo1.git
 git clone ssh://git@localhost:8822/~/repo1.git
-cd repo1
-echo Hello, Home Git server!>readme.md
-git add readme.md
-git commit -m "First commit"
-git push -u --all
-cd ..
+if [ -d ./repo1 ] ; then
+  cd repo1
+  echo Hello, Home Git server!>readme.md
+  git add readme.md && \
+  git commit -m "First commit" && \
+  git push -u --all
+  cd ..
+fi
 ssh -p 8822 git@localhost ls -lA
